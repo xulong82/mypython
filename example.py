2bb1de30f58dc1a07dbf30f262f9f204465ea7c7
Xulong Wang <emailofx@gmail.com>
2018-06-16 18:26:41 -0400

    Initial commit

---
+++ example.py
@@ -0,0 +1,11 @@
+# LPTHW exercise 
+ 
+print "Hello World!"
+my_age = 35; my_height = 180
+print "He is %s years old and %d tall." % (my_age, my_height)
+
+print """
+There's something going on here.
+With the three double-quotes.
+"""
+
